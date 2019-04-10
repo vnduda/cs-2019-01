8dcf1befd6701f885dabdae2372095786e73a1a3
vnduda <eduardavieiranetto@gmail.com>
2019-04-09 21:27:02 -0300

    Verificação da variável n

--- aula-01/java/Ex01/src/programa/Principal.java
+++ aula-01/java/Ex01/src/programa/Principal.java
@@ -1,14 +1,18 @@
 package programa;
 
 public class Principal {
-	public static boolean Propriedade3025(int n){
-		int i = n/100;
-		int j = n%100;
-		return (i+j)*(i+j) == n;
-	}
-	
-	
 	public static void main(String [] args){
 		System.out.println(Propriedade3025(3025));
 	}
+	public static boolean Propriedade3025(int n){
+		if(n>=0 || n<=9999){
+			int i = n/100;
+			int j = n%100;
+			return (i+j)*(i+j) == n;
+		}
+	
+		else 
+			return false;
+	}
+	
 }
