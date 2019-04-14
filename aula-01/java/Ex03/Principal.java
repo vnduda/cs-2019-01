38cd5b372dc02dc4fd883e195f193b0034104230
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-13 21:53:33 -0300

    Atualizado exercício 3

--- aula-01/java/Ex03/Principal.java
+++ aula-01/java/Ex03/Principal.java
@@ -2,26 +2,18 @@
 public class Principal{
     public static void main(String [] args){
         System.out.println(DiaSemana(11,4,2019));
-} 
+    } 
     public static int DiaSemana(int d, int m, int a){
         int s;
-
-        try{
-            if(d>=1 && d<=31 && m>=1 && m<=12 && a>1753){
-                if(m==1 || m==2){
-                    m+=12;
-                    a-=1;   
-                }
-                s = (d + (2*m) + (3*(m+1)/5) + a + (a/4) - (a/100) + (a/400));
-                return s%7;
-            } else {
-                throw new Exception("Erro");
-            }
-
+        if(d<1 && d>31 && m<1 && m>12 && a<1753){
+            throw new IllegalArgumentException();
+            } 
+        if(m==1 || m==2){
+            m+=12;
+            a-=1;   
         }
-        catch(Exception e){
-            System.out.println("Erro");
-        }
-        return 0;
+            s = (d + (2*m) + (3*(m+1)/5) + a + (a/4) - (a/100) + (a/400));
+            return s%7;
     }
 }
+
