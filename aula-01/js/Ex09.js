15f9cc22ccc5b36287bd84845bf66a3fa3872f54
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-16 12:49:18 -0300

    Adicionado exercício 9

---
+++ aula-01/js/Ex09.js
@@ -0,0 +1,16 @@
+function Pi(n){
+    if(n >= 1){
+        let i = 1;
+        let s = -1;
+        let d = -1;
+        let p = 0;
+        while(i <= n){
+            d = d + 2;
+            s = (-1) * s;
+            p = p + 4 * s / d;
+            i = i + 1;
+        }
+        return p;
+    }
+    return false
+}
