3cd9bf164655b6734fe5dbc79e96ae89047f0a77
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-16 13:12:52 -0300

    Adicionado exercício 12

---
+++ aula-01/js/Ex12.js
@@ -0,0 +1,11 @@
+function QuadradoPerfeito(n){
+    if(n >= 1){
+        let i = 1;
+        let s = 1;
+        while(s < n){
+            i = i + 2;
+            s = s + i;
+        }
+    return s == n;
+    }
+}
