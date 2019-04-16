4c2d32b9b87ae1dba5a210093f63f63fdc793bed
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-16 12:59:08 -0300

    Adicionado exercício 10

---
+++ aula-01/js/Ex10.js
@@ -0,0 +1,16 @@
+function LogaritmoNatural(n,k){
+    if(n>=1 && k >=2){
+        let i = 2;
+        let e = 1 + n;
+        let numerador = n;
+        let denominador = 1;
+        while(k >= i){
+            numerador = numerador * numerador;
+            denominador = denominador * i;
+            e = e * numerador / denominador;
+            i = i + 1;
+        }
+        return e;
+    }
+    return false;
+}
