de416bdd15470fd1ef0ce15346fe50fe24c4ea84
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-17 01:51:00 -0300

    Ajuste no layout e verificação de dia mês e ano

--- aula-01/js/Ex03.js
+++ aula-01/js/Ex03.js
@@ -1,13 +1,24 @@
-function DiaSemana(d, m, a){
-    let s;
-    if(d>=1 && d<=31 && m>=1 && m<=12 && a>1753){
-        if(m==1 || m==2){
-        m+=12;
-        a-=1;   
-        }
-        s = (d + (2*m) + (3*(m+1)/5) + a + (a/4) - (a/100) + (a/400));
-        return (s%7);
+function diaSemana(d, m, a){
+    
+    if (d < 1 || d > 31) {
+        throw new RangeError("Dia invalido: " + d);
     }
-    return false;
+
+    if (m < 1 || m > 12) {
+        throw new RangeError("Mês invalido: " + d);
+    }
+
+    if (a < 1753) {
+        throw new RangeError("Ano invalido: " + a);
+    }
+    let s;
+
+    if (m == 1 || m == 2) {
+        m = m + 12;
+        a = a - 1;   
+    }
+    s = (d + (2*m) + (3*(m+1)/5) + a + (a/4) - (a/100) + (a/400));
+    return (s % 7);
 }
+
     
