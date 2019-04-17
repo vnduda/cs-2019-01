905fc19da770b86b6ab8de259b8890ecc0e31c3d
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-17 01:30:57 -0300

    Implementado verificação da variável n

--- aula-01/js/Ex01.js
+++ aula-01/js/Ex01.js
@@ -1,7 +1,9 @@
 function Propriedade3025(n){
-    if(n>=0 && n<=9999){
-        let i = parseInt (n/100);
-        let j = parseInt (n%100);
-        return (i+j)*(i+j) == n;
+    if(n < 0 || n > 9999){
+        throw new RangeError();
     }
+    let i = parseInt (n / 100);
+    let j = parseInt (n % 100);
+    return (i + j) * (i + j) == n;
+    
 }
