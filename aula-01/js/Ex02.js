79fd86aa36b2c6c4dafe2242cd898d766ebb236d
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-17 01:42:09 -0300

    Mudança no layout e verificação da variável n

--- aula-01/js/Ex02.js
+++ aula-01/js/Ex02.js
@@ -1,8 +1,13 @@
-function Propriedade153(n){
-    let c = parseInt (n/100);
-    let du = parseInt (n%100);
-    let d = parseInt (du/10);
-    let u = parseInt (du%10);
+function Propriedade153(n) {
 
-    return (c*c*c)+(d*d*d)+(u*u*u) == n;
+    if(100 > n || n > 999) {
+        throw new RangeError();
+    }
+
+    let c = parseInt (n / 100);
+    let du = parseInt (n % 100);
+    let d = parseInt (du / 10);
+    let u = parseInt (du % 10);
+
+    return (c * c * c) + (d * d * d) + (u * u * u) == n;
 }
