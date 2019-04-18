183f4955d9409eefc41372b8ce966c972a7a1e4f
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-18 01:52:56 -0300

    Terminado exercício

--- aula-01/js/Ex15.js
+++ aula-01/js/Ex15.js
@@ -1,12 +1,44 @@
-function CrivoErastostenes(a[], n) {
-    
+function CrivoEratostenes(s, n) {
+
+    if(s.length > 1) {
+        throw new Error("Tamanho do array inválido");
+    }
+
+    for(let i = 0; i < s.length; i++) {
+        if (a[i] != 0) {
+            throw new RangeError("o array precisa estar zerado");
+        }
+    }
+
     if(n > 1) {
-        for(i = 2; i <= n; i++) { //checando o vetor
+
+        a = s;
+        
+        for(i = 2; i <= n; i++) {
             if(a[i] == 0) {
                 return false;
             }
         }
+
         let i = 2;
-        limite = //pesquisar como fazer
+        limite = Math.floor(Math.sqrt(n));
+        let multiplo;
+
+        while(i <= limite) {
+            
+            if(a[i] == 0) {
+                multiplo = i + i;
+                
+                while(multiplo <= n) {
+                    a[multiplo] = 1;
+                    multiplo = multiplo + i;
+                }
+
+            }
+
+            i = i + 1;
+        }
+
+        return a;
     }
 }
