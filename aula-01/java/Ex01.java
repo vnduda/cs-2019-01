279e5d7657d91db35c2cdcf03a8e7a220a9550b7
vnduda <eduardavieiranetto@gmail.com>
2019-04-18 19:30:21 -0300

    Ajustes no layout

--- aula-01/java/Ex01.java
+++ aula-01/java/Ex01.java
@@ -1,22 +1,22 @@
 public class Ex01 {
 
-	public static void main(String [] args) {
+    public static void main(String[] args) {
 		System.out.println(Propriedade3025(3025));
     }
     
 	public static boolean Propriedade3025(int n) {
         
-        if(n < 0){
+        if (n < 0) {
             throw new IllegalArgumentException("Intervalo de n inválido");
         }
 
-        if(n > 9999){
+        if (n > 9999) {
             throw new IllegalArgumentException("Intervalo de n inválido");
         }
         
-		int i = n/100;
-		int j = n%100;
-		return (i+j)*(i+j) == n;
+		int i = n / 100;
+		int j = n % 100;
+		return (i + j) * (i + j) == n;
 		
     }
 			
--- aula-01/java/Ex03.java
+++ aula-01/java/Ex03.java
@@ -1,28 +1,28 @@
 public class Ex03 {
 
-    public static void main(String [] args) {
-        System.out.println(DiaSemana(11,4,2019));
+    public static void main(String[] args) {
+        System.out.println(DiaSemana(11, 4, 2019));
     } 
 
     public static int DiaSemana(int d, int m, int a) {
         
         int s;
         
-        if(d < 1 || d > 31) {
+        if (d < 1 || d > 31) {
             throw new IllegalArgumentException("Dia inválido");
         } 
 
-        if(m < 1 || m > 12) {
+        if (m < 1 || m > 12) {
             throw new IllegalArgumentException("Mês inválido");
         } 
 
-        if(a < 1753) {
+        if (a < 1753) {
             throw new IllegalArgumentException("Ano inválido");
         } 
         
-        if(m==1 || m==2) {
-            m+=12;
-            a-=1;   
+        if (m == 1 || m == 2) {
+            m += 12;
+            a -= 1;   
         }
         
         s = (d + (2 * m) + (3 * (m + 1) / 5) + a + (a / 4) - (a / 100) + (a / 400));
