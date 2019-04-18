ad5813df4dd658bb2df02135913ab997be97381f
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-18 01:08:01 -0300

    Adicionado exercícios com verificação da variável

---
+++ aula-01/java/Ex04.java
@@ -0,0 +1,29 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println(RestoDivisao(3, 8));
+    }
+
+    public static int RestoDivisao(int x, int y) {
+        
+        int s;
+        
+        if (y < 0){
+            throw new IllegalArgumentException("Valor de y inválido");
+        }
+
+        if (x <= 0){
+            throw new IllegalArgumentException("Valor de x inválido");
+        }
+        
+        s = x;
+
+        while (y <= s) {
+            s = s - y;
+        }
+
+        return s;
+        
+        }
+        
+    }
