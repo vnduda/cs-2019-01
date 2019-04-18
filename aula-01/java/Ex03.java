4449258c80fc278496ca35f9bdb3346861a91754
kyriosdata <fabio@inf.ufg.br>
2019-04-18 15:50:57 -0300

    una declaracao e inicializacao

--- aula-01/java/Ex03.java
+++ aula-01/java/Ex03.java
@@ -6,8 +6,6 @@ public class Ex03 {
 
     public static int DiaSemana(int d, int m, int a) {
 
-        int s;
-
         if (d < 1 || d > 31) {
             throw new IllegalArgumentException("Dia inválido");
         }
@@ -25,7 +23,7 @@ public class Ex03 {
             a -= 1;
         }
 
-        s = (d + (2 * m) + (3 * (m + 1) / 5) + a + (a / 4) - (a / 100) + (a / 400));
+        int s = (d + (2 * m) + (3 * (m + 1) / 5) + a + (a / 4) - (a / 100) + (a / 400));
 
         return s % 7;
     }
