baef6b3e18b5981f92a9985b7e51373cea7ad430
kyriosdata <fabio@inf.ufg.br>
2019-04-18 15:43:15 -0300

    metodo em Java a inicial é minuscula

--- aula-01/java/Ex01.java
+++ aula-01/java/Ex01.java
@@ -2,10 +2,10 @@ public class Ex01 {
 
     // TODO ok por enquanto, mas saiba que não é assim que se testa!!!!
     public static void main(String[] args) {
-        System.out.println(Propriedade3025(3025));
+        System.out.println(propriedade3025(3025));
     }
 
-    public static boolean Propriedade3025(int n) {
+    public static boolean propriedade3025(int n) {
 
         if (n < 0 || n > 9999) {
             throw new IllegalArgumentException("Intervalo de n inválido");
@@ -14,7 +14,6 @@ public class Ex01 {
         int i = n / 100;
         int j = n % 100;
         return (i + j) * (i + j) == n;
-
     }
 
 }
