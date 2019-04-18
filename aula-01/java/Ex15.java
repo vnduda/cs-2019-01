5a47db910787e3b4702efc3fdc64e37672d73290
kyriosdata <fabio@inf.ufg.br>
2019-04-18 15:56:23 -0300

    linha desnecessaria

--- aula-01/java/Ex15.java
+++ aula-01/java/Ex15.java
@@ -12,7 +12,6 @@ public class Ex15 {
         }
 
         for (int i = 0; i < a.length; i++) {
-
             if (a[i] != 0) {
                 throw new IllegalArgumentException("O array precisa estar zerado");
             }
