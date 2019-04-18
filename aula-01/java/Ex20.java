11f9ae4fd9b39568a3a7856415d133d4159770dd
kyriosdata <fabio@inf.ufg.br>
2019-04-18 16:10:22 -0300

    melhor estrategia para solucao

--- aula-01/java/Ex20.java
+++ aula-01/java/Ex20.java
@@ -2,9 +2,16 @@ public class Ex20 {
 
     public static boolean CPF(String d) {
 
+        // TODO verifique se d é null.
+
         if (d.length() > 11 || d.length() < 11) {
             throw new IllegalArgumentException("O CPF deve conter 11 dígitos");
         }
+
+        // TODO Crie o método int[] stingParaVetorDeDigitos(String d)
+        // TODO use Character.isDigit para verificar cada caractere da string!!
+        // TODO crie int[] cpf com o retorno do método criado
+        // TODO use cpf[0] em vez de Character.getNumericValue(d.charAt(0)) e assim por diante
 
         int j = Character.getNumericValue(d.charAt(0)) + 2 * Character.getNumericValue(d.charAt(1))
                 + 3 * Character.getNumericValue(d.charAt(2)) + 4 * Character.getNumericValue(d.charAt(3))
