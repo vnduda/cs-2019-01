63fcbee072312b0503cd287f71badb614bdbeaf8
kyriosdata <fabio@inf.ufg.br>
2019-04-23 13:00:54 -0300

    simplificacao

--- aula-01/java/Ex01.java
+++ aula-01/java/Ex01.java
@@ -6,14 +6,10 @@ public class Ex01 {
     
 	public static boolean propriedade3025(int n) {
         
-        if (n < 0) {
+        if (n < 0 || n > 9999) {
             throw new IllegalArgumentException("Intervalo de n inválido");
         }
 
-        if (n > 9999) {
-            throw new IllegalArgumentException("Intervalo de n inválido");
-        }
-        
 		int i = n / 100;
 		int j = n % 100;
 		return (i + j) * (i + j) == n;
