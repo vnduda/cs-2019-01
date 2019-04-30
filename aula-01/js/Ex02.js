89cb356d9d8a4499a5274b27da1a820ed8d663fe
kyriosdata <fabio@inf.ufg.br>
2019-04-30 15:38:10 -0300

    observe uso correto de funcoes

--- aula-01/js/Ex02.js
+++ aula-01/js/Ex02.js
@@ -1,13 +1,17 @@
 function Propriedade153(n) {
 
+    // TODO verificar se null
+    // TODO verificar se numero
+    // TODO verificar se inteiro
+
     if (n < 100 || n > 999) {
         throw new RangeError("Intervalo de n inválido");
     }
 
-    const c = parseInt(n / 100);
-    const du = parseInt(n % 100);
-    const d = parseInt(du / 10);
-    const u = parseInt(du % 10);
+    const c = Math.floor(n / 100);
+    const du = n % 100;
+    const d = Math.floor(du / 10);
+    const u = du % 10;
 
     return (c**3 + d**3 + u**3) == n;
 }
