b50af10ff4af85d24f26a3d8c7b40df73a9d5a8a
Eduarda <eduardavieiranetto@gmail.com>
2019-05-14 19:29:03 -0300

    Documentação melhorada

--- aula-01/java/src/main/java/com/github/vnduda/ufg/cs/aula01/Ex04.java
+++ aula-01/java/src/main/java/com/github/vnduda/ufg/cs/aula01/Ex04.java
@@ -5,7 +5,14 @@ public class Ex04 {
     public static void main(String[] args) {
         System.out.println(restoDivisao(3, 8));
     }
-
+    
+    /**
+     * Gera o resto da divisão inteira de x por y
+     * @param x dividendo
+     * @param y divisor
+     * @throw IllegalArgumentException caso entrada esteja fora da faixa
+     * @return o resultado da divisão inteira de x por y
+    */
     public static int restoDivisao(int x, int y) {
 
         if (y < 0) {
