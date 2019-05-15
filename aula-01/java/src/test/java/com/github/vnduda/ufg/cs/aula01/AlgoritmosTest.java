ecd6db8aafa3fce0c0e61f125acc211226571f8d
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-05-15 19:12:50 -0300

    atualização import

--- aula-01/java/src/test/java/com/github/vnduda/ufg/cs/aula01/AlgoritmosTest.java
+++ aula-01/java/src/test/java/com/github/vnduda/ufg/cs/aula01/AlgoritmosTest.java
@@ -3,7 +3,7 @@ package com.github.vnduda.ufg.cs.aula01;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class AlgoritmosTest {
 
