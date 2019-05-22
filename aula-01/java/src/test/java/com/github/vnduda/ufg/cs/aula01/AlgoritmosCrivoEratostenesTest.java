49c6a07f6b9faf4701ded6e510e0ec297eaf8923
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-05-22 01:15:02 -0300

    Import desnecessário

--- aula-01/java/src/test/java/com/github/vnduda/ufg/cs/aula01/AlgoritmosCrivoEratostenesTest.java
+++ aula-01/java/src/test/java/com/github/vnduda/ufg/cs/aula01/AlgoritmosCrivoEratostenesTest.java
@@ -1,7 +1,6 @@
 package com.github.vnduda.ufg.cs.aula01;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
