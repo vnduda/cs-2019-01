91662bc78c423aa5bc278c7970fe629c82876b40
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-05-22 13:59:12 -0300

    Apagado imports desnecessários

--- aula-01/java/src/test/java/com/github/vnduda/ufg/cs/aula01/AlgoritmosCoberturaCemPorCentoTest.java
+++ aula-01/java/src/test/java/com/github/vnduda/ufg/cs/aula01/AlgoritmosCoberturaCemPorCentoTest.java
@@ -1,8 +1,6 @@
 package com.github.vnduda.ufg.cs.aula01;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
