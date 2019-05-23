92aa1a03cf0a79a95f9bf418c102c0a4460d2a7b
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-05-23 01:43:47 -0300

    NewLineAtEndOfFile ajustado

--- aula-01/java/src/main/java/com/github/vnduda/ufg/cs/aula01/package-info.java
+++ aula-01/java/src/main/java/com/github/vnduda/ufg/cs/aula01/package-info.java
@@ -1,6 +1,5 @@
 /**
- * Pacote do repositório "cs-2019-01". Contém apenas uma classe: Algoritmos, que
- * em (a){@link com.github.vnduda.ufg.cs.aula01} em que há implementações de
- * vários algoritmos matemáticos.
+ * Pacote do repositório de "cs-2019-01". Contém uma classe
+ * apenas: (a) {@link com.github.vnduda.ufg.cs.aula01}.
  */
 package com.github.vnduda.ufg.cs.aula01;
