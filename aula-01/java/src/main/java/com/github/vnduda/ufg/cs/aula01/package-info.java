519fdf2224614154eeaf2b20b6869b97487bf0c9
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-05-23 01:06:56 -0300

    Criado package-info.java

---
+++ aula-01/java/src/main/java/com/github/vnduda/ufg/cs/aula01/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote do repositório "cs-2019-01". Contém apenas uma classe: Algoritmos, que
+ * em (a){@link com.github.vnduda.ufg.cs.aula01} em que há implementações de
+ * vários algoritmos matemáticos.
+ */
+
+package com.github.vnduda.ufg.cs.aula01;
