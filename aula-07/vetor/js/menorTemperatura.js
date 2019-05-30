8e6366d9eb7871698c1d369d77d6e48ec1ea6b55
vnduda <eduardavieiranetto@gmail.com>
2019-05-30 20:23:20 -0300

    implementação Menor temperatura js

---
+++ aula-07/vetor/js/menorTemperatura.js
@@ -0,0 +1,11 @@
+/**
+ * Função para encontrar a menor temperatura em um vetor.
+ * @param vetor de valores recebidos
+ * @returns menor elemento do vetor
+ */
+function menorTemperatura (array) {
+    
+    let menorElemento = Math.min(array);
+    
+    return menorElemento;
+}
