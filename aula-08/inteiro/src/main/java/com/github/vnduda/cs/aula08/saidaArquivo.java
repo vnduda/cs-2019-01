bbbe59d200373a49f5cee062db49bf719fe4c3af
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-06-06 15:46:45 -0300

    Função para encerrar dis

--- aula-08/inteiro/src/main/java/com/github/vnduda/cs/aula08/saidaArquivo.java
+++ aula-08/inteiro/src/main/java/com/github/vnduda/cs/aula08/saidaArquivo.java
@@ -4,13 +4,14 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public final class OutputArquivo {
+public final class saidaArquivo {
 
     public static void imprimeHexadecimal(String nomeArquivo) throws IOException {
         FileInputStream fis = new FileInputStream(nomeArquivo);
         DataInputStream dis = new DataInputStream(fis);
 
         int valor = dis.readInt();
+        dis.close();
         System.out.println(Integer.toHexString(valor));
 
     }
