b5c25effa91913227535184c3c04c3286a9aca94
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-06-16 00:27:20 -0300

    Correções testes aula 07

---
+++ aula-07/vetor/java/src/test/java/com/github/vnduda/ufg/cs/aula07/AlgoritmosTest.java
@@ -0,0 +1,34 @@
+package com.github.vnduda.ufg.cs.aula07;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class AlgoritmosTest {
+    @Test
+    public void menorTemperaturaClassico() {
+        double[] vetor = {1, 2, 3, 4, 5, 6};
+        assertEquals(1, Algoritmos.menorTemperatura(vetor));
+    }
+
+    @Test
+    public void numerosImparesClassico() {
+        int[] vetor = {1};
+        int[] vetor2 = {2, 8, 10, 6, 8, 22};
+        assertEquals(1, Algoritmos.numerosImpares(vetor));
+        assertEquals(0, Algoritmos.numerosImpares(vetor2));
+    }
+
+    @Test
+    public void quantidadeElementosIguaisClassico() {
+        double[] vetor = {1, 0, 15.5};
+        assertEquals(0, Algoritmos.quantidadeElementosIguais(vetor, 5));
+    }
+
+    @Test
+    public void caractereMaisFrequenteClassico() {
+        String string = "Ola mundo!";
+        assertEquals(8, Algoritmos.caractereMaisFrequente(string));
+    }
+
+}
