62c77321e9042e4451f1ba76db26a55f1290a14d
vnduda <eduardavieiranetto@gmail.com>
2019-06-17 15:34:50 -0300

    Ajustes testes aula 07

--- aula-07/vetor/js/test/menorTemperatura.test.js
+++ aula-07/vetor/js/test/menorTemperatura.test.js
@@ -1,9 +1,9 @@
 const menorTemperatura = require("../menorTemperatura.js");
 
-test("Caso classico", () => {
-    expect(menorTemperatura([1, 2, 3, 4, 5, 6])).toBe(1);
+test("satisfaz (caso classico)", () => {
+    expect(menorTemperatura([-55.5, -100, 0.8, 200])).toBe(-100);
 });
 
 test("argumento não é vetor", () => {
-    expect(() => { menorTemperatura("abc"); }).toThrow(TypeError);
+    expect(() => { menorTemperatura([10.2, "a", 0]); }).toThrow(TypeError);
 });
--- aula-07/vetor/js/test/numerosImpares.test.js
+++ aula-07/vetor/js/test/numerosImpares.test.js
@@ -1,9 +1,9 @@
 const numerosImpares = require("../numerosImpares.js");
 
 test("Caso classico", () => {
-    expect(numerosImpares([2, 8, 10, 6, 8, 22])).toBe(0);
+    expect(numerosImpares([1, 8, 7, 3, 11])).toBe(22);
 });
 
 test("argumento não é vetor", () => {
-    expect(() => { numerosImpares("abc"); }).toThrow(TypeError);
+    expect(() => { numerosImpares([10.2, "b", 0]); }).toThrow(TypeError);
 });
