46c7c50c5295696dd42065923c71bd7d0fcece10
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-06-21 00:19:17 -0300

    Configuração Encontre

---
+++ aula-08/encontre/src/main/java/com/github/vnduda/cs/aula08/application/console/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote do repositório "cs-2019-01". Contém uma classe:
+ * {@link com.github.vnduda.cs.aula08.application.console;}.
+ * @see com.github.vnduda.cs.aula08.application.console.ProgramaEncontre
+ */
+package com.github.vnduda.cs.aula08.application.console;
+
