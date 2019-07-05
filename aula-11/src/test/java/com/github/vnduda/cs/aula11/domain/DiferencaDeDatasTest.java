1db1e972918012fbd54e9b3615b5435d8b3996d7
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-07-05 02:24:45 -0300

    Teste para DiferencaDeDatas

---
+++ aula-11/src/test/java/com/github/vnduda/cs/aula11/domain/DiferencaDeDatasTest.java
@@ -0,0 +1,25 @@
+package com.github.vnduda.cs.aula11.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiferencaDeDatasTest {
+    @Test
+    public void CasoClassicoDeDiferença() throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+        Date dataInicial = format.parse("05-07-2019");
+        Date dataFinal = format.parse("06-07-2019");    
+        assertEquals(1, DiferencaDeDatas.diferencaEntreDatas(dataInicial, dataFinal));
+    }
+
+    @Test
+    public void coberturaCemPorCento() {
+        assertNotNull(new DiferencaDeDatas());
+    }
+}
