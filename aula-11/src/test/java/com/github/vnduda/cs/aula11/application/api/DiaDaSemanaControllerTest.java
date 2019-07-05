9d72228b756f9e1229e1aebfae1a7b6ac76fdbc4
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-07-05 02:57:38 -0300

    Cobertura classe DiaDaSemanaController

---
+++ aula-11/src/test/java/com/github/vnduda/cs/aula11/application/api/DiaDaSemanaControllerTest.java
@@ -0,0 +1,16 @@
+package com.github.vnduda.cs.aula11.application.api;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiaDaSemanaControllerTest {
+    
+    @Test
+    public void coberturaCemPorCento() {
+        assertNotNull(new DiaDaSemanaController());
+    }
+}
