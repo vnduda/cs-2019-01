eb5b2326229118aa94dc69328b2963f14804d2d9
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-07-08 10:53:24 -0300

    Ajustes espaços

--- aula-12/src/main/java/com/github/vnduda/cs/aula12/domain/TransformaExtenso.java
+++ aula-12/src/main/java/com/github/vnduda/cs/aula12/domain/TransformaExtenso.java
@@ -4,8 +4,8 @@ public class TransformaExtenso {
 
     private String unidade, dezena, centena, numeroUnico;
 
-    public void escreveUnidade(int unidade){
-        switch (unidade){
+    public void escreveUnidade(int unidade) {
+        switch (unidade) {
             case 0: this.unidade = " "; break;
             case 1: this.unidade = " um"; break;
             case 2: this.unidade = " dois"; break;
@@ -20,8 +20,8 @@ public class TransformaExtenso {
         }
     }
     
-    public void escreveNumeroUnico(int numeroUnico){
-        switch(numeroUnico){
+    public void escreveNumeroUnico(int numeroUnico) {
+        switch(numeroUnico) {
             case 1: this.numeroUnico = " onze"; break;
             case 2: this.numeroUnico = " doze"; break;
             case 3: this.numeroUnico = " treze"; break;
@@ -34,8 +34,8 @@ public class TransformaExtenso {
         }
     }
     
-    public void escreveDezena(int dezena){
-        switch (dezena){
+    public void escreveDezena(int dezena) {
+        switch (dezena) {
             case 1: this.dezena = " dez"; break;
             case 2: this.dezena = " vinte"; break;
             case 3: this.dezena = " trinta"; break;
@@ -48,8 +48,8 @@ public class TransformaExtenso {
         }
     }
     
-    public void escreveCentena(int centena){
-        switch(centena){
+    public void escreveCentena(int centena) {
+        switch(centena) {
             case 1: this.centena = " cento"; break;
             case 2: this.centena = " duzentos"; break;
             case 3: this.centena = " trezentos"; break;
@@ -62,24 +62,24 @@ public class TransformaExtenso {
         }
     }
     
-    public void recuperaString(int a){
-            if(a == 0){
+    public void recuperaString(int a) {
+            if(a == 0) {
                 JOptionPane.showMessageDialog(null, "Zero.");
             }
-            else if(a < 10){
+            else if(a < 10) {
                 escreveUnidade(a);
                 JOptionPane.showMessageDialog(null, "Número = " + unidade);
             }
-            else if(a < 20){
+            else if(a < 20) {
                 int num = a % 10;
                 escreveNumeroUnico(num);
                 JOptionPane.showMessageDialog(null, "Número = " + numeroUnico);
             }
-            else if (a < 100){
+            else if (a < 100) {
                 int u = a % 10;
                 int d = a / 10;
                 String num;
-                if (d == 1){
+                if (d == 1) {
                     escreveNumeroUnico(u);
                     num = this.numeroUnico;
                 }
@@ -91,21 +91,21 @@ public class TransformaExtenso {
                 
                 JOptionPane.showMessageDialog(null, "Número = " + num);			
             }
-            else if(a < 1000){
+            else if(a < 1000) {
                 int u = a % 10;
                 int d = (a / 10) % 10;
                 int c = (a / 10) / 10;
                 String num;
-                if (d == 1){
+                if (d == 1) {
                     escreveCentena(c);
                     escreveNumeroUnico(u);
                     num = this.centena + " e" + this.numeroUnico;
                 }
-                else if (d==0){
+                else if (d==0) {
                     escreveCentena(c);
                     num = this.centena;
                 }
-                else{
+                else {
                     escreveCentena(c);
                     escreveDezena(d);
                     escreveUnidade(u);
@@ -114,7 +114,7 @@ public class TransformaExtenso {
                 
                 JOptionPane.showMessageDialog(null, "Número = " + num);
             }
-            else if(a == 1000){
+            else if(a == 1000) {
                 JOptionPane.showMessageDialog(null, "Mil.");
             }
     }
