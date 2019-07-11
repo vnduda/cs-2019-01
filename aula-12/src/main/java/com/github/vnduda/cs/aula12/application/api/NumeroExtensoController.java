3d6f7e0a822fcaab64346352159c480625d12bb8
vnduda <eduardavieiranetto@gmail.com>
2019-07-11 20:31:49 -0300

    documentação corrigida

--- aula-12/src/main/java/com/github/vnduda/cs/aula12/application/api/NumeroExtensoController.java
+++ aula-12/src/main/java/com/github/vnduda/cs/aula12/application/api/NumeroExtensoController.java
@@ -1,5 +1,7 @@
 package com.github.vnduda.cs.aula12.application.api;
 
+import java.text.ParseException;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.vnduda.cs.aula12.domain.TransformaExtenso;
 
 /**
- * Classe que possui método que retorna um número por extenso.
+ * Classe que possui método que retorna um número escrito por extenso.
  */
 @RestController
 public final class NumeroExtensoController {
@@ -23,12 +25,17 @@ public final class NumeroExtensoController {
 	@RequestMapping("ds")
 	/**
 	 * Método que obtém o objeto que retorna o número por extenso.
-	 * @param numeroExtensoStr número dado pelo usuário na interface
+	 * 
+	 * @param numeroExtensoStr
+	 *            número dado pelo usuário na interface
+	 * @throws ParseException
+	 *             exceção que sinaliza que um erro aconteceu durante a análise
 	 * @return o número escrito em extenso que é obtido pelo método recupera
-	 * String
+	 *         String
 	 */
 	public static String diaDaSemana(
-			@RequestParam(value = "numero", defaultValue = "não fornecida") final String numeroExtensoStr) {
+			@RequestParam(value = "numero", defaultValue = "não fornecida") final String numeroExtensoStr)
+			throws ParseException {
 		final int numeroExtenso = Integer.parseInt(numeroExtensoStr);
 		TransformaExtenso var = new TransformaExtenso();
 		return var.recuperaString(numeroExtenso);
